d217c2be506ddde577d2984c5a1222d4747f5a41
Alan <alanizpiriz@gmail.com>
2026-05-11 00:06:57 -0300

    menu semanal

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { getAll, getById, create, update, remove, getRandom, bulkCreate } = require('../controllers/recipesController')
+const { getAll, getById, create, update, remove, getRandom, bulkCreate, getWeekly } = require('../controllers/recipesController')
 const verificarToken = require('../middleware/auth')
 
 router.post('/bulk', verificarToken, bulkCreate)
 router.get('/',        getAll)
 router.get('/random',  getRandom)
+router.get('/weekly', getWeekly)
 router.get('/:id',     getById)
 router.post('/',       verificarToken, create)
 router.put('/:id',     verificarToken, update)
 router.delete('/:id',  verificarToken, remove)
+
 
 module.exports = router
