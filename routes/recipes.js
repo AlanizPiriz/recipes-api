985ae906835e945d24f0159b7092aa3575d85e02
Alan <alanizpiriz@gmail.com>
2026-05-21 13:41:11 -0300

    quitando fix de imagenes

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,29 +2,29 @@ const express = require('express')
 const router = express.Router()
 const { getAll, getById, create, update, remove, getRandom, bulkCreate, getWeekly } = require('../controllers/recipesController')
 const verificarToken = require('../middleware/auth')
-const Recipe = require('../models/Recipe')
+// const Recipe = require('../models/Recipe')
 
 router.post('/bulk', verificarToken, bulkCreate)
 router.get('/',        getAll)
 router.get('/random',  getRandom)
 router.get('/weekly', getWeekly)
-router.get('/fix-images', async (req, res) => {
-  try {
-    const recetas = await Recipe.find({});
+// router.get('/fix-images', async (req, res) => {
+//   try {
+//     const recetas = await Recipe.find({});
     
-    for (const receta of recetas) {
-      if (receta.image && receta.image.split('?').length > 2) {
-        const partes = receta.image.split('?');
-        receta.image = partes[0] + '?' + partes[1];
-        await receta.save();
-      }
-    }
+//     for (const receta of recetas) {
+//       if (receta.image && receta.image.split('?').length > 2) {
+//         const partes = receta.image.split('?');
+//         receta.image = partes[0] + '?' + partes[1];
+//         await receta.save();
+//       }
+//     }
     
-    res.json({ mensaje: 'Listo!', total: recetas.length });
-  } catch (error) {
-    res.json({ error: error.message });
-  }
-});
+//     res.json({ mensaje: 'Listo!', total: recetas.length });
+//   } catch (error) {
+//     res.json({ error: error.message });
+//   }
+// });
 router.get('/:id',     getById)
 router.post('/',       verificarToken, create)
 router.put('/:id',     verificarToken, update)
